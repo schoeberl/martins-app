90677e69930231780fe3427f4194bf0e009804ac
Martin Schoeberl <martin@jopdesign.com>
2016-01-28 06:47:20 +0000

    New text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render text: "This is Martin's playground for some app development."
+    render text: "This is Martin's playground for some app development. See where this goes"
   end
 end
